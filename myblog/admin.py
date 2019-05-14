5e8781521f4828c94a62c95a1584cefd15580b21
= <861865739@qq.com>
2019-05-14 19:24:55 +0800

    add comment to admin

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Article,Tag,Category,User,Likes
-from comment.models import Leaving,Expectation
+from comment.models import Leaving,Expectation,Comment
 
 admin.site.register(Article)
 admin.site.register(Tag)
@@ -10,5 +10,6 @@ admin.site.register(Category)
 admin.site.register(User)
 admin.site.register(Likes)
 
+admin.site.regiter(Comment)
 admin.site.register(Leaving)
 admin.site.register(Expectation)
